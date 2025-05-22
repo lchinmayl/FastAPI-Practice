f8fc057e7f6487fd7da727b2b601a47bac6e56d4
Sumay Chatterjee <sumaykumar234@gmail.com>
2025-05-22 23:13:29 +0530

    Practising FastAPI

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+
+app=FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Welcome to my the world of Chinmay Chatterjee"}
+
+@app.get("/posts")
+def get_posts():
+    return{"data":"This is your posts"}
+
+@app.post("/createposts")
+def createpost(payload : dict=Body(...)):
+    print(payload)
+    return{"new_post":f"title:{payload['title']} content:{payload['content']}"}
